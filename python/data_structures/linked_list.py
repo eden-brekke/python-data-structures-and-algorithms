a408c6b199aa5e6b72918b6b599da0aa0e37d673
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-04-22 16:12:16 -0700

    added comments to demonstrate understanding

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,6 +1,7 @@
 class LinkedList:
     """
-    Put docstring here
+    This class is used to create a Linked List that will initialize with an Empty head
+    It will also allow for new Nodes to be added with their own values, moving the old head-node to the next node
     """
 
     def __init__(self):
@@ -8,31 +9,39 @@ class LinkedList:
         self.head = None
 
     def insert(self, value):
-        # Create a new Node
+        # Create a new Node calling the Node class
         self.head = Node(value, self.head)
 
     def __str__(self):
+        # Take in a response and return a string of the current nodes while traversing
         response = ""
         current = self.head
         while current:
             response += f"{{ {str(current.value)} }} -> "
-            current = current.next
+            current = current.next_node
         return response + "NULL"
 
     def includes(self, value):
+        # Use this method to check if a value is currently within the Linked List
         current = self.head
 
+        # Use this while loop to step through each of the nodes within the link list
+        # While stepping through the linked list check if the value is present and return True if so.
+        # If not present while loop while break and return False
         while current:
             if current.value == value:
                 return True
-            current = current.next
+            current = current.next_node
         return False
 
 
 class Node:
-    def __init__(self, value, next=None):
+    """
+    Class to create a New Node
+    """
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.next = next
+        self.next = next_node
 
 
 class TargetError:
