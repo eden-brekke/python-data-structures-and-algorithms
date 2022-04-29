f9b53de2a83fa3a6b10ba0fdc43dbae6a18767cf
Eden Brekke <eden.lorrai@gmail.com>
2022-04-29 16:47:10 -0700

    oops I accidentally erased all my stack right before the push, now all tests pass

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -10,5 +10,23 @@ class Stack:
         # initialization here
         self.top = None
 
+    def push(self, value):
+        # method body here
+        self.top = Node(value, self.top)
 
+    def pop(self):
+        if not self.top:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        old_top = self.top
+        self.top = self.top.next
+        old_top.next = None
+        return old_top.value
+
+    def peek(self):
+        if not self.top:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
 
