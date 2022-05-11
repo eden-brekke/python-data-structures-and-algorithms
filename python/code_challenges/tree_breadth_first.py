2ba9bc4d0fe058c2114943035919eef9aaa7c622
Eden Brekke <eden.lorrai@gmail.com>
2022-05-10 19:06:06 -0700

    tests pass and cleaned up commented out code

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -10,3 +10,13 @@ def breadth_first(tree):
         return nodes
     if not tree.root:
         return nodes
+    if not breadth.front:
+        breadth.enqueue(tree.root)
+    while breadth.front:
+        root = breadth.dequeue()
+        nodes.append(root.value)
+        if root.left:
+            breadth.enqueue(root.left)
+        if root.right:
+            breadth.enqueue(root.right)
+    return (nodes)
