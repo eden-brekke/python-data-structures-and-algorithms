ab5a1bd0ceaa81a2818dd10f28ef5f2d43264956
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-27 10:05:15 -0700

    added doc strings and arguments for methods

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -7,22 +7,43 @@ class Hashtable:
         # initialization here
         pass
 
-    def hash(self):
+    def hash(self, key):
+        '''
+        Arguments: key
+        Returns: Index in the collection for that key
+        '''
         # method body here
         pass
 
-    def set(self):
+    def set(self, key, value):
+        '''
+        Arguments: key, value
+        Returns: nothing
+        This method should hash the key, and set the key and value pair in the table, handling collisions as needed.
+        Should a given key already exist, replace its value from the value argument given to this method.
+        '''
         # method body here
         pass
 
-    def get(self):
+    def get(self, key):
+        '''
+        Arguments: key
+        Returns: Value associated with that key in the table
+        '''
         # method body here
         pass
 
-    def contains(self):
+    def contains(self, key):
+        '''
+        Arguments: key
+        Returns: Boolean, indicating if the key exists in the table already.
+        '''
         # method body here
         pass
 
     def keys(self):
+        '''
+        Returns: Collection of keys
+        '''
         # method body here
         pass
