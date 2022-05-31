c851477f3e4752fb00bbdbb7269686e58eab3a2a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-31 16:58:02 -0700

    added tree intersection functionality

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,19 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
+def tree_intersection(tree_a, tree_b):
+    matching_values = set()
 
-def tree_intersection():
-    pass
+    hashtable = Hashtable()
+
+    tree_a_values = BinaryTree.pre_order(tree_a)
+    tree_b_values = BinaryTree.pre_order(tree_b)
+
+    for i in tree_a_values:
+        hashtable.set(i, i)
+
+    for i in tree_b_values:
+        if hashtable.contains(i):
+            matching_values.add(i)
+
+    return matching_values
