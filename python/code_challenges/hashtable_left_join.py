5cb7a29be83b8946843bda9d027187c7ebb9d3dd
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-01 14:47:36 -0700

    code works! tests pass! complete

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,19 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(table_a, table_b):
+    '''
+    Arguments: Two hashmaps
+    Return: a List with the keys of table a and the values from both table a and table b (if available)
+    '''
+    joined_list = []
+
+    for key, value in table_a.items():
+        if table_b.get(key):
+            joined_list.append([key, value, table_b.get(key)])
+
+        else:
+            joined_list.append([key, value, 'NONE'])
+
+    print(joined_list)
+    return joined_list
