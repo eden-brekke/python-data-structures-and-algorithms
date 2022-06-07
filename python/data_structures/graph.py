1aeda2cb8caec9e904bd9b5657b4dd263a14dc47
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-06 17:22:54 -0700

    imported queue, changed get_nodes slightly and added breadth first method

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,3 +1,5 @@
+from data_structures.queue import Queue
+
 class Graph:
     """
     Create a graph of Vertices
@@ -34,7 +36,7 @@ class Graph:
         Arguments: none
         Returns all of the nodes in the graph as a collection (set, list, or similar)
         '''
-        return self._adjacency_list.keys() # returns the keys within the dictionary that is adjacency list
+        return list(self._adjacency_list.keys()) # returns the keys within the dictionary that is adjacency list
 
 
     def get_neighbors(self, vertex):
@@ -51,6 +53,25 @@ class Graph:
         Returns the total number of nodes in the graph
         '''
         return len(self._adjacency_list) # return the length of the dictionary
+
+
+
+    def breadth_first(self, vertex):
+        all_vertices = []
+        breadth = Queue()
+        visited_vertices = set()
+        breadth.enqueue(vertex)
+        visited_vertices.add(vertex)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            all_vertices.append(front.value)
+
+            for neighbor in self.get_neighbors(front):
+                if neighbor.vertex not in visited_vertices:
+                    visited_vertices.add(neighbor.vertex)
+                    breadth.enqueue(neighbor.vertex)
+        return all_vertices
 
 class Vertex:
     '''
